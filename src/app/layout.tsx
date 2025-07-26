9fe7c7c1f845bdcc456f562a6e893e942c660d49
Rodrigo Silva <rodrigo.walace.silva@gmail.com>
2025-07-26 19:54:08 -0300

    feat(auth): Prover contexto de autenticacao globalmente
    Inclui AuthProvider no layout principal para que o contexto de autenticação esteja disponível em toda a aplicação.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 // import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { AntdRegistry } from "@ant-design/nextjs-registry";
+import { AuthProvider } from "~/contexts/AuthContext";
 import StyledComponentsRegistry from "../../lib/registry";
 
 // const geistSans = Geist({
@@ -28,7 +29,9 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <AntdRegistry>
-          <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+          <StyledComponentsRegistry>
+            <AuthProvider>{children}</AuthProvider>
+          </StyledComponentsRegistry>
         </AntdRegistry>
       </body>
     </html>
