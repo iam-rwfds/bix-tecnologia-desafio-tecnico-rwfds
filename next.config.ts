5d9624af56bb3fdb944ea10e47bef40d37e28ae4
Rodrigo Silva <rodrigo.walace.silva@gmail.com>
2025-07-26 22:23:55 -0300

    build(nextjs): Configurar saída standalone para otimizar deploy

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-	/* config options here */
-	compiler: {
-		styledComponents: true,
-	},
+  /* config options here */
+  compiler: {
+    styledComponents: true,
+  },
+  output: "standalone",
 };
 
 export default nextConfig;
