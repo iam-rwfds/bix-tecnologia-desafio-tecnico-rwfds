792003999de996b3b8a0eb485f04865493c35f42
Rodrigo Silva <rodrigo.walace.silva@gmail.com>
2025-07-27 11:07:47 -0300

    style(ui): Adiciona cursor pointer e padroniza estilo de botões

--- src/app/acesso/cadastro/components/styles.ts
+++ src/app/acesso/cadastro/components/styles.ts
@@ -46,13 +46,11 @@ export const Input = styled(InputBase)`
 `;
 
 export const Button = styled.button`
-width: 100%;
+  width: 100%;
+  background-color: oklch(54.6% 0.245 262.881);
+  border: none;
+  padding: 0.75rem 1rem;
+  border-radius: 0.5rem;
 
-background-color: oklch(54.6% 0.245 262.881);
-
-border: none;
-
-padding: 0.75rem 1rem;
-
-border-radius: 0.5rem;
+  cursor: pointer;
 `;
